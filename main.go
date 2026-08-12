c0d1f167b1a8ecebe961d0a17e9fdd71ff03e7eb
Galberton <gustavo.borghezan030706@gmail.com>
2026-08-12 14:19:30 -0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"smartcommit/ai"
+	"smartcommit/cli"
+	"smartcommit/git"
+)
+
+func main() {
+	diff, err := git.GetStagedDiff()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	AnthropicProvider, err := ai.NewAnthropicProvider()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	var provider ai.Provider = AnthropicProvider
+
+	for {
+		fmt.Println("Analisando mudanças...")
+
+		msg, err := provider.GenerateCommitMessage(diff)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		action := cli.ShowSuggestion(msg)
+
+		switch action {
+		case cli.Accept:
+			commit(msg)
+			return
+		case cli.Edit:
+			fmt.Print("Digite a nova mensagem")
+			reader := bufio.NewReader(os.Stdin)
+			newMsg, _ := reader.ReadString('\n')
+			msg = strings.TrimSpace(newMsg)
+			commit(msg)
+			return
+
+		case cli.Regenerate:
+			continue
+
+		case cli.Cancel:
+			fmt.Println("Cancelado")
+			os.Exit(0)
+		}
+	}
+}
+
+func commit(msg string) {
+	cmd := exec.Command("git", "commit", "-m", msg)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println(string(output))
+		os.Exit(1)
+	}
+	fmt.Printf("Comit criado: %s\n", msg)
+}
